30c6b219b53947796ca1aa5398bed7fd7bc63656
rdiarod228-spec <rdiarod228@g.educaand.es>
2026-05-07 19:51:56 +0200

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,47 @@
-basic.forever(function () {
-	
-})
+enum SentidoMovimiento {
+    //% block="avanzando"
+    Avanzando,
+    //% block="retrocediendo"
+    Retrocediendo
+}
+
+enum LadoGiro {
+    //% block="derecha"
+    Derecha,
+    //% block="izquierda"
+    Izquierda
+}
+
+//% weight=100 color=#0fbc11 icon="\uf1b9" block="RoMaqueen"
+namespace maqueenFacil {
+
+    /**
+     * Hace girar el coche Maqueen avanzando o retrocediendo hacia un lado.
+     */
+    //% block="girar %sentido hacia %lado"
+    //% weight=100
+    export function girar(sentido: SentidoMovimiento, lado: LadoGiro): void {
+
+        let velocidad = 80
+
+        if (sentido == SentidoMovimiento.Avanzando && lado == LadoGiro.Derecha) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+            maqueen.motorStop(maqueen.Motors.M2)
+        }
+
+        if (sentido == SentidoMovimiento.Avanzando && lado == LadoGiro.Izquierda) {
+            maqueen.motorStop(maqueen.Motors.M1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
+        }
+
+        if (sentido == SentidoMovimiento.Retrocediendo && lado == LadoGiro.Derecha) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, velocidad)
+            maqueen.motorStop(maqueen.Motors.M2)
+        }
+
+        if (sentido == SentidoMovimiento.Retrocediendo && lado == LadoGiro.Izquierda) {
+            maqueen.motorStop(maqueen.Motors.M1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velocidad)
+        }
+    }
+}
